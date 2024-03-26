3cca7b0c0e1f373201610a8fff4d87ce7dc761c8
Usuario <Usuario@CPD02-M18>
2024-03-26 12:25:24 -0300

    12:25

--- Aula26032024/Program.cs
+++ Aula26032024/Program.cs
@@ -10,10 +10,63 @@ namespace Aula26032024
     {
         static void Main(string[] args)
         {
-            int num =0;
-            while (num!=100) { 
-                num++;
-                Console.WriteLine(num);
+            //int num =0;
+            //while (num!=100) { 
+            //    num++;
+            //    Console.WriteLine(num);
+            //}
+            //Console.ReadKey();
+
+            //int num = 100;
+            //while (num != 0)
+            //{
+            //    num--;
+            //    Console.WriteLine(num);
+            //}
+            //Console.ReadKey();
+
+            //int num = 0;
+            //while (num != 102)
+            //{
+            //    Console.WriteLine(num);
+            //    num +=2;
+            //}
+            //Console.ReadKey();
+
+            //int mult = 0;
+            //Console.WriteLine("Numero para multiplicar: ");
+            //mult = int.Parse(Console.ReadLine());
+
+            //int cont = mult;
+
+            //while (cont <= 100)
+            //{
+            //    Console.WriteLine(cont);
+            //    cont += mult;
+            //}
+            //Console.ReadKey();
+
+            Console.WriteLine("PARES OU IMPARES?:");
+            string n = Console.ReadLine();
+            int nn = 0;
+
+            while (n == "P") 
+            {
+                while (nn != 22) {
+                    Console.WriteLine(nn);
+                    nn+=2;
+                    n = "A";
+                }
+            }
+            while (n == "I")
+            {
+                nn = 1;
+                while (nn <= 20)
+                {
+                    Console.WriteLine(nn);
+                    nn += 2;
+                    n = "A";
+                }
             }
             Console.ReadKey(); 
         }
