dc0953d3cb5db433d125953115ec255dc971dc81
Usuario <Usuario@CPD02-M18>
2024-03-26 10:25:38 -0300

    Adicionar arquivos de projeto.

---
+++ Aula26032024/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula26032024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num =0;
+            while (num!=100) { 
+                num++;
+                Console.WriteLine(num);
+            }
+            Console.ReadKey(); 
+        }
+    }
+}
